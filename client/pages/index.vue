1668958719fbe289fc67e7c843e6f5cf9e4b30c9
lancemiraflorcube@gmail.com <lancemiraflorcube@gmail.com>
2023-08-25 15:52:23 +0800

    fix: updated

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,12 +1,10 @@
 <template>
-	<p class="text-yellow-500">0</p>
-	<p class="text-white text-6xl">Main body dashboard</p>
-	<p>{{ data?.ships?.length || 0 }}</p>
-	<p class="text-2xl text-blue-500">
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita perspiciatis officia ut pariatur
-		ipsa quis quod earum. Iusto omnis sint iste amet, consequuntur quia eos dolorum consectetur
-		perferendis obcaecati aliquid?
-	</p>
+	<div>
+		<p class="text-yellow-500">0</p>
+		<p class="text-white text-6xl">Main body dashboard</p>
+		<p>{{ data?.ships?.length || 0 }}</p>
+		<p class="text-2xl text-blue-500">asd</p>
+	</div>
 </template>
 
 <script>
